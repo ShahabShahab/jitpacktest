180e50226ac3476a8817b12791ad85eabed49414
shahabgolpaiegandev <shahabgolpaiegan@gmail.com>
2018-05-27 10:23:46 +0430

    first commit

---
+++ app/src/main/java/com/faranegar/jitpacktest/Human.java
@@ -0,0 +1,17 @@
+package com.faranegar.jitpacktest;
+
+/**
+ * Created by shahab on 5/27/2018...
+ */
+
+public class Human {
+    private String name = "";
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
